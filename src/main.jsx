6ff10452cfa429269b6bc74fc9d50920b0343e1e
misafxd <pisichu.mafs@gmail.com>
2025-02-20 12:07:49 -0600

    Add router provider and routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Layout from './Layout.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Favorites from './Favorites.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <App />
+      },
+      {
+        path: 'favorites',
+        element: <Favorites />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
